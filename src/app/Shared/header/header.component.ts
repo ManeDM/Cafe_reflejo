f66dee5395139cace24a9e5b7864ab8cacbc710c
ManeDM <mane657@hotmail.com.com>
2022-12-16 20:19:28 -0500

    correciones finales funcioanlidad menu hamburguesa

--- src/app/Shared/header/header.component.ts
+++ src/app/Shared/header/header.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
-
-@Component({
+import { Component,
+  OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+  
+  @Component({
   selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
-})
-export class HeaderComponent implements OnInit {
-  menuButton: ElementRef;
-  menu: ElementRef;
-
-  constructor (private elementRef: ElementRef)
-
-  ngOnInit() {
-    this.menuButton = document.querySelector('menu-button');
-    this.menu = document.querySelector('.list__items');
-    this.menuButton.addEventListener('click', () => {
-      this.menu.classList.toggle('.open');
-    });
+  })
+  export class HeaderComponent implements AfterViewInit {
+  
+  @ViewChild('menuButton', { static: true }) menuButton!: ElementRef;
+  @ViewChild('menuItems', { static: true })  menu!: ElementRef;
+  
+  constructor() { }
+  
+  ngAfterViewInit() {
+  this.menuButton.nativeElement.addEventListener('click', () => {
+  this.menu.nativeElement.classList.toggle('open')
+  });
   }
-}
+  }
