78d7223536b113f5532a9b9469815b09a00e47c4
ManeDM <mane657@hotmail.com.com>
2022-12-19 14:58:31 -0500

    Responsividad del menu hamburguesa

--- src/app/Shared/header/header.component.ts
+++ src/app/Shared/header/header.component.ts
@@ -1,21 +1,21 @@
-import { Component,
-  OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
-  
-  @Component({
+import { Component, OnInit } from '@angular/core';
+
+@Component({
   selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
-  })
-  export class HeaderComponent implements AfterViewInit {
-  
-  @ViewChild('menuButton', { static: true }) menuButton!: ElementRef;
-  @ViewChild('menuItems', { static: true })  menu!: ElementRef;
-  
+})
+export class HeaderComponent implements OnInit {
+
+  navClass: string = "menu-closed"; // Inicialmente, el menú está cerrado y se aplica la clase
+  hidden: boolean = true; // Inicialmente, la lista de elementos del menú está oculta
+
   constructor() { }
-  
-  ngAfterViewInit() {
-  this.menuButton.nativeElement.addEventListener('click', () => {
-  this.menu.nativeElement.classList.toggle('open')
-  });
+
+  ngOnInit() { }
+
+  toggleMenu() {
+    this.navClass = this.navClass === "menu-closed" ? "" : "menu-closed";
+    this.hidden = !this.hidden; // Al hacer clic en el botón, se muestra o se oculta la lista de elementos
   }
-  }
+}
