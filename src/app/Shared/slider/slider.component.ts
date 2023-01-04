4ec11e6394ae7cae4d0080e65d16db86707a7974
ManeDM <mane657@hotmail.com.com>
2023-01-03 20:45:21 -0500

    Cleaning the slider's code

--- src/app/Shared/slider/slider.component.ts
+++ src/app/Shared/slider/slider.component.ts
@@ -9,60 +9,24 @@ import { Component, OnInit,  } from "@angular/core";
 })
 
 export class SliderComponent implements OnInit {
-  slideIndex = 1;
-
-  ngOnInit(): void {
-
-    this.showSlides();
-    setInterval(() => {
-    const slides: NodeListOf<HTMLElement> = document.querySelectorAll('.slide');
-
-      for (let i = 0; i < slides.length; i++) {
-        if (slides[i].style.left === '0px') {
-          if (i === slides.length - 1) {
-            this.slideIndex = 1;
-          } else {
-            this.slideIndex = i + 2;
-          }
-          break;
-        }
-      }
-
-      this.showSlides();
-    }, 3000);
-  }
-
-  showSlides() {
-    const slides: NodeListOf<HTMLElement> = document.querySelectorAll('.slide');
-    const dots: NodeListOf<HTMLElement> = document.querySelectorAll('.dot');
+  currentSlide = 0;
   
-    // Oculta todas las imágenes
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
-    }
-  
-    // Muestra la imagen seleccionada
-    slides[this.slideIndex - 1].style.display = 'block';
-  
-    // Actualiza la clase 'active' del punto
-    dots.forEach(dot => dot.classList.remove('active'));
-    dots[this.slideIndex - 1].classList.add('active');
-  
+  slides = [
+    'assets/property/property_01.jpeg',
+    'assets/property/property_02.jpeg',
+    'assets/property/property_03.jpeg'
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
   }
 
   prevSlide() {
-    this.slideIndex--;
-    if (this.slideIndex < 1) {
-      this.slideIndex = 3;
-    }
-    this.showSlides();
+    this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
   }
-  
+
   nextSlide() {
-    this.slideIndex++;
-    if (this.slideIndex > 3) {
-      this.slideIndex = 1;
-    }
-    this.showSlides();
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
   }
 }
