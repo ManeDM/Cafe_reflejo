0cd8b78f0c9704382495f0d9bbfd6bf8f75ced53
ManeDM <mane657@hotmail.com.com>
2023-01-06 17:45:08 -0500

    adding header's styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router'
 import { CarritoComponent } from '../app/Shared/carrito/carrito.component'
+import { AboutUsComponent } from './Shared/about-us/about-us.component';
+import { HeroSectionComponent } from './Shared/hero-section/hero-section.component';
 import { ListCartComponent } from './Shared/list-cart/list-cart.component';
 
 const routes: Routes = [
@@ -8,6 +10,16 @@ const routes: Routes = [
   {path:'',component:CarritoComponent},
 
   {path:'lista_carrito',component:ListCartComponent},
+
+  {path:'Inicio',component:HeroSectionComponent},
+
+  {path:'Productos',component:CarritoComponent},  
+
+  {path:'Conocenos',component:AboutUsComponent},
+  
+  {path:'Contacto',component:AboutUsComponent}
+
+  
 ];
 
 @NgModule({
